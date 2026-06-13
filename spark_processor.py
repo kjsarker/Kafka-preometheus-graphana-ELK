3a9a967d57e1bf28b0a390d44fb582e3284de08b
kjsarker <kollolsust@gmail.com>
2026-06-12 22:27:12 -0500

    Adding code to create spark session

--- spark_processor.py
+++ spark_processor.py
@@ -8,3 +8,15 @@ AGGREGATES_TOPIC = "transaction_aggregates"
 ANOMALIES_TOPIC = "transactions_anomalies"
 CHECKPOINT_DIR = "/mnt/spark-checkpoints"
 STATES_DIR = "/mnt/spark-state"
+
+# Creating spark session, wired up with Kafka support, checkpointing, and tuned partition counts for a streaming pipeline that reads financial transactions.
+# .appName names the application. This label appears in the Spark UI and logs so you can identify this job.
+# Defauld partition is 200. We're doing 5 here.
+# Checkpoints store progress (offsets read, watermarks) so Spark can recover from crashes without reprocessing everything. 
+spark = SparkSession.builder \
+    .appName("FinancialTransactionProcessor") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3") \
+    .config("spark.sql.streaming.checkpointLocation", CHECKPOINT_DIR)\
+    .config("spark.sql.shuffle.partitions", 5) \
+    .config("spark.sql.streaming.stateStore.stateStoreDir", STATES_DIR) \
+    .getOrCreate()
